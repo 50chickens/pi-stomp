c95d2f664c9681f8ae9d222e2887cfdd284114ad
50chickens <50chickens@gmail.com>
2025-09-30 07:46:21 +1000

    reorder

--- setup.ps1
+++ setup.ps1
@@ -16,15 +16,14 @@ $sudoFoldersToCreate = @("/usr/mod/scripts")
 $userFoldersToCreate = @("Speaker Cabinets IRs", "Reverb IRs", "Audio Loops", "Audio Recordings", "Audio Samples", "Audio Tracks", "MIDI Clips", "MIDI Songs", "Hydrogen Drumkits", "SF2 Instruments", "SFZ Instruments", "Amplifier Profiles", "Aida DSP Models", "NAM Models")
 
 Test-ICanSudo
+Invoke-PackageInstall #packages that we need to install.
 
 New-Folders -foldersToCreate $foldersToCreate -baseFolder "~"
 New-Folders -foldersToCreate $sudoFoldersToCreate -sudo
 New-Folders -foldersToCreate $userFoldersToCreate -baseFolder "~/data/user-files"
 
-
 New-PythonVenv -venvPath "~/.env"
 bash python-venv.sh #run this bash script to setup python venv.
-Invoke-PackageInstall #packages that we need to install.
 Invoke-InstallMod
 
 if ($installLv2plugins)
